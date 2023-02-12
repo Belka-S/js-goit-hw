a3ad87d0d20d2671dca020dcb7cb5a7860786a5b
Belka-S <belka.srg@gmail.com>
2023-02-12 17:09:52 +0100

    add task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import { throttle } from 'lodash';
+import Vimeo from '@vimeo/player';
 
+// Vimeo player
+const iframe = document.querySelector('iframe#vimeo-player');
+const player = new Vimeo(iframe);
+// console.log(player);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(e) {
+  localStorage.setItem(STORAGE_KEY, e.seconds);
+}
