431796fd972f68f17c0b62dd8fe83fb79f6555ae
Belka-S <belka.srg@gmail.com>
2023-02-17 22:54:34 +0100

    add task-2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,21 +2,6 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import 'flatpickr/dist/themes/material_blue.css';
 
-// Milliseconds converter
-function convertMs(ms) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
-
 // Elements
 const inputEl = document.querySelector('input#datetime-picker');
 const startBtnEl = document.querySelector('button[data-start]');
@@ -34,7 +19,7 @@ let timerId = null;
 let selectedDateMs = null;
 const SELECTED_DATE = 'selectedDateMs';
 
-// Date selector
+// Select date
 const fp = flatpickr(inputEl, {
   enableTime: true,
   time_24hr: true,
@@ -47,36 +32,12 @@ const fp = flatpickr(inputEl, {
 
 function checkSelectedDate(selectedDates) {
   selectedDateMs = selectedDates[0].getTime();
-  console.log(selectedDateMs);
   if (selectedDateMs <= Date.now()) {
     startBtnEl.disabled = true;
     removeAlert();
     return makeAlert('Please choose a date in the future');
   }
   startBtnEl.disabled = false;
-}
-
-// Alert message
-function makeAlert(message) {
-  const alert = document.createElement('p');
-  alert.className = 'alert';
-  alert.textContent = `${message}`;
-  alert.style.backgroundColor = 'red';
-  alert.style.color = 'white';
-  alert.style.fontSize = '24px';
-  alert.style.display = 'inline-block';
-  alert.style.margin = '10px';
-  alert.style.padding = '10px';
-  startBtnEl.nextElementSibling.after(alert);
-}
-
-function removeAlert() {
-  setTimeout(() => {
-    const alertRemain = document.querySelector('p.alert');
-    if (alertRemain) {
-      alertRemain.remove();
-    }
-  }, 3000);
 }
 
 // Set timer on Button
@@ -90,6 +51,8 @@ function setTimer() {
       Object.keys(timerRef).forEach(
         el => (timerRef[el].textContent = `${timeObj[el]}`.padStart(2, '0'))
       );
+    } else {
+      stopTimer();
     }
   }, 1000);
 
@@ -105,7 +68,6 @@ function setTimer() {
 addEventListener('load', onLoad);
 
 function onLoad() {
-  console.log(localStorage.getItem(SELECTED_DATE));
   if (localStorage.getItem(SELECTED_DATE)) {
     selectedDateMs = localStorage.getItem(SELECTED_DATE);
     setTimer();
@@ -132,4 +94,42 @@ function makeStopBtn() {
 
   stopBtnEl = document.querySelector('button[data-stop]');
   stopBtnEl.addEventListener('click', stopTimer);
+}
+
+// Milliseconds converter
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+// Alert message
+function makeAlert(message) {
+  const alert = document.createElement('p');
+  alert.className = 'alert';
+  alert.textContent = `${message}`;
+  alert.style.backgroundColor = 'red';
+  alert.style.color = 'white';
+  alert.style.fontSize = '24px';
+  alert.style.display = 'inline-block';
+  alert.style.margin = '10px';
+  alert.style.padding = '10px';
+  startBtnEl.nextElementSibling.after(alert);
+}
+
+function removeAlert() {
+  setTimeout(() => {
+    const alertRemain = document.querySelector('p.alert');
+    if (alertRemain) {
+      alertRemain.remove();
+    }
+  }, 3000);
 }
