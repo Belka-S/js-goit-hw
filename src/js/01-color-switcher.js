0f9236d5bc29460efab12e68028a1eded6a6d64b
Belka-S <belka.srg@gmail.com>
2023-02-17 09:31:28 +0100

    add task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const ref = {
+  startBtnEl: document.querySelector('button[data-start]'),
+  stopBtnEl: document.querySelector('button[data-stop]'),
+  bodyEl: document.querySelector('body'),
+};
+// Object.values(ref).forEach(el => console.log(el));
 
+ref.startBtnEl.addEventListener('click', onStartClick);
+ref.stopBtnEl.addEventListener('click', onStopClick);
+
+ref.stopBtnEl.disabled = true;
+let intervalId = null;
+
+function onStartClick() {
+  ref.stopBtnEl.disabled = false;
+  ref.startBtnEl.disabled = true;
+  intervalId = setInterval(() => {
+    ref.bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStopClick() {
+  ref.stopBtnEl.disabled = true;
+  ref.startBtnEl.disabled = false;
+  clearInterval(intervalId);
+}
